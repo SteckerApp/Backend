523a5f0298f2d687e35e8521b68fc1fe4f93726a
Temitope Soge <sogets@tasued.edu.ng>
2022-08-08 21:12:20 +0100

    attach and activate user subscription

--- app/Http/Controllers/UserSubscriptionController.php
+++ app/Http/Controllers/UserSubscriptionController.php
@@ -13,26 +13,41 @@ use App\Trait\HandleResponse;
 class UserSubscriptionController extends Controller
 {
     use HandleResponse;
-    public function store(Request $request)
+    public function attachUserSubscription(Request $request)
     {
         $this->validate($request, [
             'subscription_id' => 'required',
-            'reference' => 'required',
+            // 'reference' => 'required',
             'duration' => 'required|in:monthly,bi-annually,annually',
         ]);
 
         $record = UserSubscription::create([
             'user_id' => auth()->user()->id,
             'subscription_id' => $request->subscription_id,
-            'reference' => $request->reference,
             'duration' => $request->duration,
             'payment_date' => Carbon::now(),
             'start_date' => Carbon::now(),
             'end_date' => Carbon::now()->addMonth(),
-            'status' => 'active',
+            'payment_status' => 'pending',
+            'status' => 'inactive',
+
         ]);
 
         return $this->successResponse($record, 'User Subscription created successfully', 201);
 
+    }
+    public function activatePayment(Request $request, $id)
+    {
+        $this->validate($request, [
+            'reference' => 'required',
+        ]);
+        $record = UserSubscription::find($id);
+        $record->reference = $request->reference;
+        $record->payment_status = "paid";
+        $record->status = "active";
+
+        $record = $record->save();
+
+        return $this->successResponse($record, 'User Subscription activated successfully', 200);
     }
 }
