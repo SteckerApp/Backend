4745e60b5bdb9b1cf0dfc5aa3d132acb42d8fa90
Temitope Soge <sogets@tasued.edu.ng>
2022-09-27 11:01:05 +0100

    brand_documents seeder

--- database/seeders/BrandDocumentTableSeeder.php
+++ database/seeders/BrandDocumentTableSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class BrandDocumentTableSeeder extends Seeder
 {
@@ -15,11 +16,10 @@ class BrandDocumentTableSeeder extends Seeder
     public function run()
     {
         $brand_documents = array(
-            array('id' => '1','brand_id' => '1','upload' => '/storage/companies/brands/4/documents1.jpg','user_id' => '1','created_at' => '2022-09-26 21:22:22','updated_at' => '2022-09-26 21:22:22'),
-            array('id' => '2','brand_id' => '1','upload' => '/storage/companies/brands/4/documentsdog1.jpg','user_id' => '1','created_at' => '2022-09-26 21:22:22','updated_at' => '2022-09-26 21:22:22'),
-            array('id' => '3','brand_id' => '1','upload' => '/storage/companies/brands/4/documentshunninghake2009.pdf','user_id' => '1','created_at' => '2022-09-26 21:22:22','updated_at' => '2022-09-26 21:22:22')
+            array('id' => '1','brand_id' => '1','upload' => '/storage/companies/brands/2/documents1.jpg','user_id' => '1','created_at' => '2022-09-27 09:56:10','updated_at' => '2022-09-27 09:56:10'),
+            array('id' => '2','brand_id' => '2','upload' => '/storage/companies/brands/2/documentsdog1.jpg','user_id' => '1','created_at' => '2022-09-27 09:56:10','updated_at' => '2022-09-27 09:56:10'),
+            array('id' => '3','brand_id' => '2','upload' => '/storage/companies/brands/2/documentshunninghake2009.pdf','user_id' => '1','created_at' => '2022-09-27 09:56:10','updated_at' => '2022-09-27 09:56:10')
           );
-
           DB::table('brand_documents')->insert($brand_documents);
     }
 }
