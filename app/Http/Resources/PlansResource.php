cd0feaef43d2d50771043732dc5f9bd4d3f21f83
Temitope Soge <temz@Temitopes-MacBook-Pro.local>
2023-09-17 19:17:25 +0100

    group identifier

--- app/Http/Resources/PlansResource.php
+++ app/Http/Resources/PlansResource.php
@@ -19,6 +19,7 @@ class PlansResource extends JsonResource
         return [
             'subscription_id' => $this->id,
             'name' => $this->title,
+            'group_identifier' => $this->group_identifier,
             'show_in_catalogue' => $this->visible,
             'most_popular' => $this->most_popular,
             'price' => $this->price,
