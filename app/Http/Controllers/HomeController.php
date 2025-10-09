803a7f4543192df9925de9b8d977614006cdae03
emmawhocoded <emmawhocoded@gmail.com>
2025-10-09 15:29:38 +0100

    added migration

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Country;
+use Illuminate\Http\Request;
+use App\Models\Configuration;
+use App\Services\HomeService;
+use App\Trait\HandleResponse;
+use App\Traits\HttpResponses;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Artisan;
+
+class HomeController extends Controller
+{
+    use HandleResponse;
+
+    public function migrate(): JsonResponse
+    {
+        Artisan::call('migrate', [
+            '--force' => true
+        ]);
+
+        return response()->json([
+            'message' => 'Migration completed successfully.',
+            'output' => Artisan::output(),
+        ]);
+    }
+}
